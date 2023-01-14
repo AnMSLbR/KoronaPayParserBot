a92cf29fcc1cb2ae286bf86f5c2250222ddecb24
Ildar Mindiyarov <77970650+AnMSLbR@users.noreply.github.com>
2023-01-14 03:16:57 +0300

    Feature-4. Combining parsed information in CombineParsedInfo() method. Handling of Update CallbackQuery.

--- KoronaPayParserBot/Program.cs
+++ KoronaPayParserBot/Program.cs
@@ -1,4 +1,6 @@
 ﻿using KoronaPayParserLib;
+using Microsoft.VisualBasic;
+using System.Diagnostics.Metrics;
 using System.Net.Http.Headers;
 using Telegram.Bot;
 using Telegram.Bot.Exceptions;
@@ -61,7 +63,7 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
             }
             InlineKeyboardMarkup currencyKeyboard = new(new[]{currencyKeys});
 
-            Message sentMessage = await botClient.SendTextMessageAsync(chatId, $"Destination country: {messageText.Substring(1)} \r\nSelect a currency:", replyMarkup: currencyKeyboard, cancellationToken: cancellationToken);
+            Message sentMessage = await botClient.SendTextMessageAsync(chatId, $"Destination country: {messageText} \r\nSelect a currency:", replyMarkup: currencyKeyboard, cancellationToken: cancellationToken);
         }
     }
 
@@ -69,7 +71,25 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
     {
         if (update.CallbackQuery.Data == "Update")
         {
-
+            var entityValues = update.CallbackQuery.Message.EntityValues.ToArray();
+            var country = entityValues[0].Substring(1);
+            var currency = entityValues[1];
+            try
+            {
+                parser.Parse(country, currency);
+                string parsedInfo = CombineParsedInfo(country, currency);
+                Message editMessage = await botClient.EditMessageTextAsync(update.CallbackQuery.Message.Chat.Id,
+                                                                           update.CallbackQuery.Message.MessageId,
+                                                                           text: parsedInfo,
+                                                                           parseMode: ParseMode.MarkdownV2,
+                                                                           replyMarkup: terminalKeyboard,
+                                                                           cancellationToken: cancellationToken);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Message sentMessage = await botClient.SendTextMessageAsync(update.CallbackQuery.Message.Chat.Id, "Bad request", cancellationToken: cancellationToken);
+            }
         }
         else if (update.CallbackQuery.Data == "Return")
         {
@@ -84,12 +104,10 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
             try
             {
                 parser.Parse(country, currency);
+                string parsedInfo = CombineParsedInfo(country, currency);
                 Message sentMessage = await botClient.SendTextMessageAsync(update.CallbackQuery.Message.Chat.Id,
-                                                                           $"Exchange rate: {parser.GetExchangeRate()}\r\n" +
-                                                                           $"Transfer amount: {parser.GetReceivingAmount()} {parser.GetReceivingCurrency()}\r\n" +
-                                                                           $"Transfer amount without commision: {parser.GetSendingAmountWithoutCommission()} {parser.GetSendingCurrency()}\r\n" +
-                                                                           $"Commission: {parser.GetSendingCommission()} {parser.GetSendingCurrency()}\r\n" +
-                                                                           $"Total transfer amount: {parser.GetSendingAmount()} {parser.GetSendingCurrency()}\r\n",
+                                                                           text: parsedInfo,
+                                                                           parseMode: ParseMode.MarkdownV2,
                                                                            replyMarkup: terminalKeyboard,
                                                                            cancellationToken: cancellationToken);
             }
@@ -112,5 +130,17 @@ Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, Cancell
     };
     Console.WriteLine(ErrorMessage);
     return Task.CompletedTask;
+}
+
+string CombineParsedInfo(string country, string currency)
+{
+    var requestDateTime = $"{DateTime.UtcNow.Month.ToString("00")}\\-{DateTime.UtcNow.Day.ToString("00")}\\-{DateTime.UtcNow.Year.ToString("00")} " +
+                   $"{DateTime.UtcNow.TimeOfDay.ToString("hh\\:mm\\:ss")}";
+    return $"Date: {requestDateTime} UTC\r\n" +
+           $"Destination country: /{country}\r\n" +
+           $"Currency: __{currency}__\r\n" +
+           $"Exchange rate: {parser.GetExchangeRate()}\r\n" +
+           $"Transfer amount: {parser.GetReceivingAmount()} {parser.GetReceivingCurrency()}\r\n" +
+           $"Total transfer amount: {parser.GetSendingAmount()} {parser.GetSendingCurrency()}\r\n";
 }
  
